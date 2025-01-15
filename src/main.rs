5fd87147dac6bf914543746466e437564c57e835
Priscila A. <105573463+PriscahC@users.noreply.github.com>
2025-01-15 11:05:26 +0300

    update function

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn read_version(transaction_hex: &str) -> u32 {
 }
 fn main() {
     // obtained the hex fro here (https://blockstream.info/testnet/tx/3c1804567a336c3944e30b3c2593970bfcbf5b15a40f4fc6b626a360ee0507f2?expand)
-    let version: u32 = read_version(transaction_hex:"3c1804567a336c3944e30b3c2593970bfcbf5b15a40f4fc6b626a360ee0507f2")
+    // let version: u32 = read_version(transaction_hex:"3c1804567a336c3944e30b3c2593970bfcbf5b15a40f4fc6b626a360ee0507f2")
+    let version: u32 = read_version("3c1804567a336c3944e30b3c2593970bfcbf5b15a40f4fc6b626a360ee0507f2");
     println!("version:{}", version);
 }
